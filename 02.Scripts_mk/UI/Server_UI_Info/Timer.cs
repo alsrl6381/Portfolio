3dce5a392c3a9401cddb11db7379c09b59f8ba4d
alsrl6381 <65713403+alsrl6381@users.noreply.github.com>
2025-05-08 12:00:31 +0900

    Update Timer.cs

--- 02.Scripts_mk/UI/Server_UI_Info/Timer.cs
+++ 02.Scripts_mk/UI/Server_UI_Info/Timer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+//게임 라운드 시간을 동기화하고 화면에 출력하는 기능
 public class Timer : NetworkBehaviour
 {
     [Networked] TickTimer timer { get; set; }
